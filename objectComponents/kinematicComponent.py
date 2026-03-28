c141475e887d66127098e2c835c95238373ae9f0
AnFa222 <anthony.imad.fares.123@gmail.com>
2026-03-28 10:17:59 +0200

    Kinematic Fixes

--- objectComponents/kinematicComponent.py
+++ objectComponents/kinematicComponent.py
@@ -5,21 +5,25 @@ class Kinematic:
         self.velocity_x = 0
         self.velocity_y = 0
         self.gravity = 0
-        self.gravity_direction = 0
+        self.gravity_direction = 90
         self.friction = 0
+        self.on_ground = False 
 
     def update(self, thisObject, engine):
-        self.velocity_x += self.gravity * math.cos(math.radians(self.gravity_direction)) * engine.deltaTime
-        self.velocity_y += self.gravity * math.sin(math.radians(self.gravity_direction)) * engine.deltaTime
-        self.velocity_x -= self.friction * self.velocity_x * engine.deltaTime
-        self.velocity_y -= self.friction * self.velocity_y * engine.deltaTime
+        dt = engine.deltaTime
+
+        if not self.on_ground:
+            self.velocity_x += self.gravity * math.cos(math.radians(self.gravity_direction)) * dt
+            self.velocity_y += self.gravity * math.sin(math.radians(self.gravity_direction)) * dt
+
+        self.velocity_x -= self.friction * self.velocity_x * dt
+        self.velocity_y -= self.friction * self.velocity_y * dt
 
         if not thisObject.collider:
-            thisObject.transform.x += self.velocity_x * engine.deltaTime
-            thisObject.transform.y += self.velocity_y * engine.deltaTime
+            thisObject.transform.x += self.velocity_x * dt
+            thisObject.transform.y += self.velocity_y * dt
             return
 
-        dt = engine.deltaTime
         original_x = thisObject.transform.x
         original_y = thisObject.transform.y
 
@@ -32,5 +36,8 @@ class Kinematic:
         thisObject.transform.y += self.velocity_y * dt
         collisions_y = engine.check_collision(thisObject, engine.objects)
         if collisions_y:
+            self.on_ground = True
             thisObject.transform.y = original_y
-            self.velocity_y = 0 
+            self.velocity_y = 0
+        else:
+            self.on_ground = False
