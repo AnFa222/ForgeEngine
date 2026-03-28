af2187a3f484cd094273b22323e5e2448a8f606a
AnFa222 <anthony.imad.fares.123@gmail.com>
2026-03-28 09:37:16 +0200

    Gravity and friction

--- objectComponents/kinematicComponent.py
+++ objectComponents/kinematicComponent.py
@@ -1,9 +1,19 @@
+import math
+
 class Kinematic:
     def __init__(self):
         self.velocity_x = 0
         self.velocity_y = 0
+        self.gravity = 0
+        self.gravity_direction = 0
+        self.friction = 0
 
     def update(self, thisObject, engine):
+        self.velocity_x += self.gravity * math.cos(math.radians(self.gravity_direction)) * engine.deltaTime
+        self.velocity_y += self.gravity * math.sin(math.radians(self.gravity_direction)) * engine.deltaTime
+        self.velocity_x -= self.friction * self.velocity_x * engine.deltaTime
+        self.velocity_y -= self.friction * self.velocity_y * engine.deltaTime
+
         if not thisObject.collider:
             thisObject.transform.x += self.velocity_x * engine.deltaTime
             thisObject.transform.y += self.velocity_y * engine.deltaTime
